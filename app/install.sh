9697f55c8e6efed0e06eb78bede03b95f8ce70c2
Zeng Weiyu <zerozwy@gmail.com>
2023-08-11 22:47:25 +0800

    Update install.sh

--- app/install.sh
+++ app/install.sh
@@ -12,9 +12,9 @@ pip3 install -U pip
 # install basic packages
 pip3 install -r $APPDIR/requirements.txt
 # install forked version of efb-wechat-slave
-pip3 install --no-deps -e git+https://github.com/RoyXiang/efb-wechat-slave.git@v2.0.7.post3#egg=efb-wechat-slave
-# install efb-filter-middleware
-pip3 install --no-deps -e git+https://github.com/zeXtreme/efb-filter-middleware.git#egg=efb-filter-middleware
+pip3 install --no-deps -e git+https://github.com/HoganGolden/efb-wechat-slave-itchat-uos.git#egg=efb-wechat-slave-itchat-uos
+# install forked version of efb-patch-middleware
+pip3 install --no-deps -e git+https://github.com/HoganGolden/efb-patch-middleware-itchat-uos.git#egg=efb-patch-middleware-itchat-uos
 # deactivate virtual environment
 pip3 uninstall -y pip wheel
 deactivate
